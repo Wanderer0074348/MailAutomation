cb16fa6416b666f269f24a83809f8eff20484da0
Wanderer0074348 <tanaymatta27@gmail.com>
2023-11-15 12:51:39 +0400

    Added some code

---
+++ main.py
@@ -0,0 +1,5 @@
+# Hello this is a python script to pull mail data from gmail and segregate it into different folders so that event and teacher mails are seperate and understsandable 
+# done using Google API and python version 3.12.10
+
+
+print("Hello World")
